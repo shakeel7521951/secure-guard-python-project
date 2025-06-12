d74ecb2c01c0fb83aa168b329fb6c1fe42065c3e
Muhammad Shakeel <shakeel7521951@gmail.com>
2025-06-12 16:42:23 +0500

    update debug and allow hosts

--- secureGuard/settings.py
+++ secureGuard/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-my8c7r*mxa%eo23@n3u=vd2qp=jmia6&l^x=h@4y^voj+f86r&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["shakeeldeveloper.pythonanywhere.com"]
 
 
 # Application definition
